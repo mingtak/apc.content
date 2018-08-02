d6cac2cf38b48dd5484321db7150aa0c041f79bc
Andy Fang <andy@mingtak.com>
2018-08-02 14:31:31 +0000

    "Add vocabulary: Language"

---
+++ src/apc/content/tests/test_vocab_language.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from apc.content import _
+from apc.content.testing import APC_CONTENT_INTEGRATION_TESTING  # noqa
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from zope.component import getUtility
+from zope.schema.interfaces import IVocabularyFactory
+from zope.schema.interfaces import IVocabularyTokenized
+
+import unittest
+
+
+class LanguageIntegrationTest(unittest.TestCase):
+
+    layer = APC_CONTENT_INTEGRATION_TESTING
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+
+    def test_vocab_language(self):
+        vocab_name = 'apc.content.Language'
+        factory = getUtility(IVocabularyFactory, vocab_name)
+        self.assertTrue(IVocabularyFactory.providedBy(factory))
+
+        vocabulary = factory(self.portal)
+        self.assertTrue(IVocabularyTokenized.providedBy(vocabulary))
+        self.assertEqual(
+            vocabulary.getTerm('sony-a7r-iii').title,
+            _(u'Sony Aplha 7R III'),
+        )
