6c2309196a82376f966090ffe13c4d49cdae8e4c
Andy Fang <andy@mingtak.com>
2018-08-14 13:38:36 +0000

    update liveclass

--- src/apc/content/content/live_class.py
+++ src/apc/content/content/live_class.py
@@ -8,12 +8,45 @@ from plone.supermodel.directives import fieldset
 from z3c.form.browser.radio import RadioFieldWidget
 from zope import schema
 from zope.interface import implementer
+from z3c.relationfield.schema import RelationList, RelationChoice
+from plone.app.vocabularies.catalog import CatalogSource
 from apc.content import _
 
 
 class ILiveClass(model.Schema):
     """ Marker interface and Dexterity Python Schema for LiveClass
     """
+
+    localLang = schema.TextLine(
+        title=_(u'Local Language'),
+        required=True,
+    )
+
+    teachSchool = RelationChoice(
+        title=_(u"Teach School"),
+        required=True,
+        source=CatalogSource(Type='School')
+    )
+
+    coSchool = RelationList(
+        title=_(u"Training Center"),
+        default=[],
+        value_type=RelationChoice(title=_(u"Related"),
+            source=CatalogSource(Type='School')
+        ),
+        required=False,
+    )
+
+    teacher = RelationChoice(
+        title=_(u"Teacher"),
+        required=True,
+        source=CatalogSource(Type='Teacher')
+    )
+
+    embeded = schema.Text(
+        title=_(u'Embeded Code'),
+        required=False,
+    )
 
     # directives.widget(level=RadioFieldWidget)
     # level = schema.Choice(
