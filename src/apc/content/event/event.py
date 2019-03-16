410cccaff535592245ce475dd4bd6eb5e27a006e
Andy Fang <andy@mingtak.com.tw>
2019-03-16 23:40:38 +0800

    add updateDate event

--- src/apc/content/event/event.py
+++ src/apc/content/event/event.py
@@ -1,6 +1,24 @@
 # -*- coding: utf-8 -*-
 from plone import api
+from Products.CMFPlone.utils import safe_unicode
 from apc.content import _
+
+import datetime
+
+
+def updateDate(obj, event):
+    course = obj.id.split('_')[0]
+    year = obj.id.split('_')[1]
+    month = obj.title.split(safe_unicode('月'))[0]
+    day = obj.title.split(safe_unicode('月'))[1].split(safe_unicode('日'))[0]
+    tzinfo = obj.start.tzinfo
+
+    obj.start = datetime.datetime(int(year), int(month), int(day), 8, 0, tzinfo=tzinfo)
+    obj.end = datetime.datetime(int(year), int(month), int(day), 9, 0, tzinfo=tzinfo)
+
+    newId = '%s_%s_%s_%s' % (course, year, month, day)
+    api.content.rename(obj=obj, new_id=newId, safe_id=True)
+    obj.reindexObject()
 
 
 def moveObjectsToTop(obj, event):
